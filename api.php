be63858d0b6c95d89095cb1e688f52e0a95ea2f9
Anam-magnus <80564052+Anam-magnus@users.noreply.github.com>
2021-03-13 11:19:52 +0530

    api

---
+++ api.php
@@ -0,0 +1,30 @@
+<?php		
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+	
+	require_once('AngelBroking.php');
+
+	//require_once(AngelConfigrationManage.php);
+
+	//echo(AngelBroking::GenerateSession() . '<br />');
+	AngelBroking::GenerateToken();
+	//AngelBroking::GetProfile();
+	//AngelBroking::LogOut();
+	//AngelBroking::GetRMS();
+	//echo(AngelBroking::PlaceOrder() );
+	//AngelBroking::ModifyOrder();
+	//AngelBroking::CancelOrder();
+	//AngelBroking::GetOrderBook();
+	//echo (AngelBroking::GetTradeBook());
+	//echo (AngelBroking::CancelOrder());
+	//echo (AngelBroking::ModifyOrder());
+	//AngelBroking::GetHoldings();
+	//AngelBroking::GetPosition();
+	//AngelBroking::ConvertPosition();
+	//AngelBroking::CreateRule();
+	//AngelBroking::ModifyRule();
+	//AngelBroking::CancelRule();
+	//AngelBroking::RuleDetails();
+	//AngelBroking::RuleList();
+?>
